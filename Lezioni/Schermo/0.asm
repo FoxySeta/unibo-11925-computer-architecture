748febc7026c5d5b1709aa48b718e05f0b83f778
FoxySeta <stefano.volpe2@studio.unibo.it>
2020-11-03 12:45:46 +0100

    Lezione 03/11

---
+++ Lezioni/Schermo/0.asm
@@ -0,0 +1,40 @@
+// Scrivere un programma Hack che traccia una riga verticale in colonna 20
+// lunga 50 pixel
+
+@SCREEN
+D = A
+@screen_loc
+M = D + 1 // prima locazione: SCREEN + 0 x 32 + 20 / 16
+
+@50 // numero di righe
+D = A
+@counter
+M = D
+
+(LOOP)
+    @counter
+    D = M
+    @END
+    D;JLE
+
+    @16 // 20° bit della riga: 2 ^ (20 % 16)
+    D = A
+    @screen_loc
+    A = M
+    M = D
+
+    @screen_loc
+    D = M
+    @32 // incremento riga
+    D = D + A
+    @screen_loc
+    M = D
+    @counter
+    M = M - 1
+
+    @LOOP
+    0;JMP
+
+(END)
+@END
+0;JMP
---
+++ Lezioni/Schermo/1.asm
@@ -0,0 +1,53 @@
+// Scrivere un programma Hack che traccia sullo schermo una linea diagonale
+// (a 45 gradi) he parte dall'angolo in altro a sinistra lunga 50 px
+
+@SCREEN
+D = A
+@screen_loc
+M = D // prima locazione: SCREEN + 0 x 32 + 0 / 16
+
+@screen_val
+M = 1 // 1° bit della riga: 2 ^ (0 % 16) 
+
+@50 // numero di righe
+D = A
+@counter
+M = D
+
+(LOOP)
+    @counter
+    D = M
+    @END
+    D;JLE
+
+    @screen_val
+    D = M
+    @screen_loc
+    A = M
+    M = D
+
+    @screen_val // incremento riga
+    D = M
+    MD = M + D
+    @NOT_OVERFLOW
+    D;JNE
+        @screen_val // overflow
+        M = 1
+        @screen_loc
+        M = M + 1
+    (NOT_OVERFLOW)
+    @screen_loc
+    D = M
+    @32 // incremento riga
+    D = D + A
+    @screen_loc
+    M = D
+    @counter
+    M = M - 1
+
+    @LOOP
+    0;JMP
+
+(END)
+@END
+0;JMP
